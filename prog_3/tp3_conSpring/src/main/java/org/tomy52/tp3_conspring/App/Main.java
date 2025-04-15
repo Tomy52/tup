4ae79814dafcd2450959c0433544d6d2472ffe10
Tomas Silva <toto9116@gmail.com>
2025-04-14 21:17:08 -0300

    Cambio en anotacion

--- prog_3/tp3_conSpring/src/main/java/org/tomy52/tp3_conspring/App/Main.java
+++ prog_3/tp3_conSpring/src/main/java/org/tomy52/tp3_conspring/App/Main.java
@@ -3,11 +3,11 @@ package org.tomy52.tp3_conspring.App;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import org.tomy52.tp3_conspring.View.Menu;
 
-@SpringBootApplication
+@Component
 @ComponentScan(basePackages = "org.tomy52.tp3_conspring")
 public class Main implements CommandLineRunner {
     private final Menu menu;
